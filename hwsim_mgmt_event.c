476c5f63545e89f735819ef3eb43fd1ab25b4e88
Patrick Grosse <patrick.grosse@uni-muenster.de>
2016-12-29 19:51:25 +0100

    Reduced waiting time for first radio

--- hwsim_mgmt_event.c
+++ hwsim_mgmt_event.c
@@ -5,6 +5,8 @@
 
 #include "hwsim_mgmt_event.h"
 
+static pthread_mutex_t nl_cb_mutex;
+
 static void notify_device_creation(int id) {
     printf("Created device with ID %d\n", id);
     exit(EXIT_SUCCESS);
@@ -15,23 +17,42 @@ static void notify_device_deletion() {
     exit(EXIT_SUCCESS);
 }
 
+static int nl_msg_cb(struct nl_msg *msg, void *rctx) {
+    hwsim_cli_ctx *ctx = rctx;
+    struct nlmsghdr *nlh = nlmsg_hdr(msg);
+    struct genlmsghdr *gnlh = nlmsg_data(nlh);
+    int retcode = gnlh->cmd;
+    if (retcode == 0) {
+        if (!pthread_mutex_trylock(&nl_cb_mutex)) {
+            if (ctx->args.mode == HWSIM_OP_CREATE) {
+                notify_device_creation(0);
+            } else if (ctx->args.mode == HWSIM_OP_DELETE) {
+                notify_device_deletion();
+            }
+            // should not be needed
+            pthread_mutex_unlock(&nl_cb_mutex);
+        }
+    }
+    return 0;
+}
+
 static int nl_err_cb(struct sockaddr_nl *nla, struct nlmsgerr *nlerr, void *rctx) {
     UNUSED(nla);
     hwsim_cli_ctx *ctx = rctx;
-    if (ctx->args.mode == HWSIM_OP_DELETE) {
-        if (nlerr->error == -ENODEV) {
-            fprintf(stderr, "Device not found\n");
-        } else {
-            fprintf(stderr, "Unknown error with id %d\n", nlerr->error);
+    if (!pthread_mutex_trylock(&nl_cb_mutex)) {
+        if (ctx->args.mode == HWSIM_OP_DELETE) {
+            if (nlerr->error == -ENODEV) {
+                fprintf(stderr, "Device not found\n");
+            } else {
+                fprintf(stderr, "Unknown error with id %d\n", nlerr->error);
+            }
+            exit(EXIT_FAILURE);
+        } else if (ctx->args.mode == HWSIM_OP_CREATE) {
+            notify_device_creation(nlerr->error);
         }
-        exit(EXIT_FAILURE);
-    } else if (ctx->args.mode == HWSIM_OP_CREATE) {
-        notify_device_creation(nlerr->error);
+        // should not be needed
+        pthread_mutex_unlock(&nl_cb_mutex);
     }
-    struct genlmsghdr *gnlh = nlmsg_data(&nlerr->msg);
-    fprintf(stderr, "ERR ID WAS %d\n", nlerr->error);
-    fprintf(stderr, "nl: cmd %d, seq %d: %s\n", gnlh->cmd,
-            nlerr->msg.nlmsg_seq, strerror(abs(nlerr->error)));
     return NL_SKIP;
 }
 
@@ -54,10 +75,15 @@ static void *run_nl_event_dispatcher(void *rctx) {
 }
 
 int register_event(hwsim_cli_ctx *ctx) {
-    if (nl_cb_err(ctx->nl_ctx.cb, NL_CB_CUSTOM, nl_err_cb, ctx)) {
+    if (nl_cb_set(ctx->nl_ctx.cb, NL_CB_MSG_IN, NL_CB_CUSTOM, nl_msg_cb, ctx)) {
         fprintf(stderr, "Error on callback registration\n");
         return -1;
     }
+    if (nl_cb_err(ctx->nl_ctx.cb, NL_CB_CUSTOM, nl_err_cb, ctx)) {
+        fprintf(stderr, "Error on callback registration (2)\n");
+        return -1;
+    }
+
     pthread_t libe_thread;
     return pthread_create(&libe_thread, NULL, run_nl_event_dispatcher, ctx);
 }
